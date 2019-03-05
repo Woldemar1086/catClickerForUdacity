ab486ad8e4b3d48a39ce3fd56ad3e74ca2b62a33
Woldemar1086 <khodyrev.v.a@gmail.com>
2019-03-05 18:26:59 +0100

    Refactor source code according to MVC pattern

--- js/script.js
+++ js/script.js
@@ -1,46 +1,52 @@
 (function() {
-
-
-    var listWithCats = document.querySelectorAll('.listOfCats')[0],
-        resetButton = document.querySelectorAll('.resetButton')[0];
+    let model, view, controller;
 
     model = {
         cats: {
             cat1: {
                 url: './img/cat1.jpg',
                 name: 'cat1',
-                numberOfclicks: 0,
-                srcNumber: 1
+                numberOfClicks: 0,
+                srcNumber: 1,
+                adminStatus: false
             },
             cat2: {
                 url: './img/cat2.jpg',
                 name: 'cat2',
-                numberOfclicks: 0,
-                srcNumber: 2
+                numberOfClicks: 0,
+                srcNumber: 2,
+                adminStatus: false
             },
             cat3: {
                 url: './img/cat3.jpg',
                 name: 'cat3',
-                numberOfclicks: 0,
-                srcNumber: 3
+                numberOfClicks: 0,
+                srcNumber: 3,
+                adminStatus: false
             },
             cat4: {
                 url: './img/cat4.jpg',
                 name: 'cat4',
-                numberOfclicks: 0,
-                srcNumber: 4
+                numberOfClicks: 0,
+                srcNumber: 4,
+                adminStatus: false
             },
             cat5: {
                 url: './img/cat5.jpg',
                 name: 'cat5',
-                numberOfclicks: 0,
-                srcNumber: 5
+                numberOfClicks: 0,
+                srcNumber: 5,
+                adminStatus: false
             }
         },
-        incrementClickForCat: function(catEl){
-            this.cats['cat' + catEl.dataset.number].numberOfclicks += 1;
+        incrementClickForCat: function(catEl) {
+            this.cats['cat' + catEl.dataset.number].numberOfClicks += 1;
+        },
+
+        toggleAdminStatus: function(cat) {
+            cat.adminStatus = !cat.adminStatus;
         }
-    }
+    };
 
     controller = {
         init: function() {
@@ -53,20 +59,29 @@
             this.onResetClick();
         },
         onListClick: function() {
-            var me = this;
+            let me = this,
+                listWithCats = view.getDomElement('.listOfCats')[0];
+
             listWithCats.addEventListener('click', function(e) {
-                cat = e.target
+                let cat = e.target;
+
                 if (cat.dataset.number) {
                     me.incrementNumberOfClicks(cat);
                 }
             });
         },
+
         onResetClick: function() {
-            var me = this;
+            let me = this,
+                resetButton = view.getDomElement('.resetButton')[0];
+
             resetButton.addEventListener('click', me.resetAll);
         },
 
-        getCats: function (){
+        getCats: function(cat) {
+            if (cat) {
+                return model.cats[cat];
+            }
             return model.cats;
         },
         // Increment clicks amount
@@ -81,28 +96,104 @@
 
         // Reset number of clicks
         resetAll: function() {
-            var cats = model.cats
+            let cats = model.cats;
             for (let cat in cats) {
-                cats[cat].numberOfclicks = 0;
+                if(cats.hasOwnProperty(cat)) {
+                    cats[cat].numberOfClicks = 0;
+                }
             }
             view.render();
+        },
+
+        changeAdminStatus: function(cat) {
+            model.toggleAdminStatus(cat);
         }
-    }
+    };
 
     view = {
+        toggleClass: function(element, className) {
+            if (!element || !className) {
+                return;
+            }
+
+            let classString = element.className,
+                nameIndex = classString.indexOf(className);
+            if (nameIndex === -1) {
+                classString += ' ' + className;
+            } else {
+                classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+            }
+            element.className = classString;
+        },
+
+        getDomElement: function(querySlector) {
+            return document.querySelectorAll(querySlector);
+        },
+
         // Update on page number of clicks
         // @param {DOMElement/Object} catEl DOMElement <img>
         render: function(catEl) {
-            var cats = controller.getCats();
+            let cats = controller.getCats();
 
             if (catEl) {
-                document.querySelectorAll(`.catListText${catEl.dataset.number}`)[0].textContent = cats[`cat${catEl.dataset.number}`].numberOfclicks;
+                let catNumber = `cat${catEl.dataset.number}`;
+                this.getDomElement(`.catListText${catEl.dataset.number}`)[0].textContent = cats[catNumber].numberOfClicks;
             } else {
                 for (let cat in cats) {
-                    document.querySelectorAll(`.catListText${cats[cat].srcNumber}`)[0].textContent = cats[cat].numberOfclicks
+                    if (cats.hasOwnProperty(cat)) {
+                        this.getDomElement(`.catListText${cats[cat].srcNumber}`)[0].textContent = cats[cat].numberOfClicks
+                    }
+
                 }
             }
         },
+
+        changeAdminInfoVisibility: function(cat, container, classActive) {
+            if (cat.adminStatus) {
+                this.toggleClass(container, classActive);
+            }
+        },
+
+        createDOMElement: function(tag, newClassName, content) {
+            let newElement;
+            
+            if (tag && newClassName) {
+                newElement = document.createElement(tag);
+                newElement.className = newClassName;
+                if (content) {
+                    newElement.textContent = content;
+                }
+                return newElement;
+            }
+        },
+
+        createAdminInfo: function(catInfo) {
+            let adminContainer = this.createDOMElement('div', 'adminContainer'),
+                rowName = this.createDOMElement('span', 'adminNameRow', 'Name: '),
+                rowUrl = this.createDOMElement('span', 'adminUrlRow', 'Image URL: '),
+                rowClicks = this.createDOMElement('span', 'adminClicksRow', '#Clicks: ');
+
+            [rowName, rowUrl, rowClicks].forEach(function(element) {
+                adminContainer.appendChild(element)
+            });
+            this.getDomElement('.cat__container')[0].appendChild(adminContainer);
+            this.updateAdminInfo(catInfo);
+            this.changeAdminInfoVisibility(catInfo, adminContainer, 'active')
+        },
+
+        updateAdminInfo: function(catInfo) {
+            let me = this;
+
+            me.getDomElement('.adminContainer .adminNameRow')[0].appendChild(me.createDOMElement('span', 'value', catInfo.name));
+            me.getDomElement('.adminContainer .adminUrlRow')[0].appendChild(me.createDOMElement('span', 'value', catInfo.url));
+            me.getDomElement('.adminContainer .adminClicksRow')[0].appendChild(me.createDOMElement('span', 'value', catInfo.numberOfClicks));
+        },
+
+        cleanMainCatContainer: function() {
+            this.getDomElement('.cat__container')[0].innerHTML = '';
+        },
+
+
 
         // Create image with given `url` and `alt` text
         // @param {String} url Image source url
@@ -110,7 +201,7 @@
         // @return {DOMElement/Object} DOM element <img>
 
         createImg: function(url, name) {
-            var img = new Image();
+            let img = new Image();
             img.src = url;
             img.alt = name;
             img.className = `cat ${name}`;
@@ -120,42 +211,44 @@
 
         // Create DOM elements for every child in object `cats`
         // @param {Object} config with all settings
-        init: function(config) {
-            var img,
+        init: function() {
+            let img,
                 counter = 0,
                 cats = controller.getCats(),
-                catListContainer = document.querySelectorAll('.listOfCats')[0];
+                catListContainer = this.getDomElement('.listOfCats')[0],
+                createImgContainer, createCounterForImg;
 
             for (let cat in cats) {
-                counter += 1;
+                if(cats.hasOwnProperty(cat)) {
+                    counter += 1;
 
-                creatImgContaner = document.createElement('div');
-                creatImgContaner.className = 'catList__inner__container';
+                    createImgContainer = this.createDOMElement('div', 'catList__inner__container');
 
-                creatCounterForImg = document.createElement('span');
-                creatCounterForImg.className = `catListText catListText${counter}`;
+                    createCounterForImg = this.createDOMElement('span', `catListText catListText${counter}`);
 
 
-                img = this.createImg(cats[cat].url, cats[cat].name);
-                img.dataset.number = counter;
+                    img = this.createImg(cats[cat].url, cats[cat].name);
+                    img.dataset.number = counter;
 
-                creatImgContaner.appendChild(img);
-                creatImgContaner.appendChild(creatCounterForImg);
-                catListContainer.appendChild(creatImgContaner);
+                    createImgContainer.appendChild(img);
+                    createImgContainer.appendChild(createCounterForImg);
+                    catListContainer.appendChild(createImgContainer);
+                }
             }
         },
         setCatToMainWindow: function(catEl) {
             if (catEl) {
-                mainViewCat = document.querySelectorAll('.cat__container .catList__inner__container')[0];
-                parentNode = catEl.parentElement;
-                document.querySelectorAll('.cat__container')[0].appendChild(parentNode);
-                listWithCats.appendChild(mainViewCat);
+                let parentNode = catEl.parentElement.cloneNode(true);
+                this.cleanMainCatContainer();
+                this.getDomElement('.cat__container')[0].appendChild(parentNode);
+                this.createAdminInfo(controller.getCats(`cat${catEl.dataset.number}`));
+
             } else {
-                parentNode = document.querySelectorAll('.catList__inner__container')[0];
-                document.querySelectorAll('.cat__container')[0].appendChild(parentNode);
+                let parentNode = this.getDomElement('.catList__inner__container')[0].cloneNode(true);
+                this.getDomElement('.cat__container')[0].appendChild(parentNode);
             }
         }
-    }
+    };
 
     controller.init()
 })();
